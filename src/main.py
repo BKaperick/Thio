f7701a6820b49846a994002875856c868c8b5640
bkaperick <bryankaperick@gmail.com>
2017-08-09 17:53:57 -0400

    Correct move proportion has jumped from .483 to .976 with a change in how move pairs are generated, which I believe resolved a bug where the end of one game and the start of another could be paired.

--- src/main.py
+++ src/main.py
@@ -2,25 +2,30 @@ from read_pgn import *
 from chess import *
 from itertools import chain
 
-def gen_pairs(arr):
-    team = 1
-    for i in range(len(arr)-1):
-        yield arr[i], arr[i+1], team
-        team *= -1
+def gen_pairs(games):
+    '''
+    arr is a list of game states
+    '''
+    for game in games:
+        states = game.runGame()
+        team = 1
+        for i in range(len(states)-1):
+            yield states[i], states[i+1], team
+            team *= -1
 
 if __name__ == "__main__":
     games = parsePGN(fname)
     games = only_correct_games(fname, games)
     
-    states = []
-    for game in games:
-        states += game.runGame()
+    #states = []
+    #for game in games:
+    #    states += game.runGame()
 
     #chain([games[ind].runGame() for ind in game_indices])
 
     correct = 0
     total = 0
-    for start, end, team in gen_pairs(states):
+    for start, end, team in gen_pairs(games):
         result = is_valid_move(start, end, team)
         correct += int(result)
         total += 1
