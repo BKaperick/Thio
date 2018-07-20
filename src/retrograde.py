f67cc7423f051306b23731f26ae2fc8d9901ba0a
bkaperick <bryankaperick@gmail.com>
2018-07-20 21:51:53 +0200

    Ehh done.

--- src/retrograde.py
+++ src/retrograde.py
@@ -1,5 +1,32 @@
 from chess import *
 
+def piece_count(board, team):
+    piece_locs = zip(*np.where(board*team > 0))
+    expected_piece_count = {
+            P: 8,
+            R: 2,
+            N: 2,
+            B: 2,
+            K: 1,
+            Q: 1,
+            empty: 1
+    }
+    '''
+    {board: [], start: (piece, row, col), end: (piece, row, col)}
+    ('enpassant', row, col)
+    ('OO', kingrow, kingcol)
+    ('OOO', kingrow, kingcol)
+    '''
+
+    #enemy_pieces_missing = {K,Q,B,N,R,P,fP}
+    for row, col in piece_locs:
+        enemy_piece = on_board(board, row, col)
+        piece_val = abs(piece) if abs(piece) != fP else P
+        expected_piece_count[piece_val] -= 1
+    
+    piece_types_left = {k for k,v in expected_piece_count if v > 0}
+    return piece_types_left
+
 def retrograde(board, team, nmoves = 1):
     '''
     INPUT
@@ -22,47 +49,70 @@ def retrograde(board, team, nmoves = 1):
     (3) Special moves like castling/enpassant
     '''
     
-    # TODO
-    # * create set() of all pieces which have been removed from -`team`'s side
-    enemy_piece_locs = zip(*np.where(board*team < 0))
-    expected_piece_count = {
-            P: 8,
-            R: 2,
-            N: 2,
-            B: 2,
-            K: 1,
-            Q: 1,
-            empty: 1
-    }
-    
-    
+    piece_types_left = piece_count(board, -team)
 
-    #enemy_pieces_missing = {K,Q,B,N,R,P,fP}
-    for row, col in enemy_piece_locs:
-        enemy_piece = on_board(board, row, col)
-        piece_val = abs(enemy_piece) if abs(enemy_piece) != fP else P
-        expected_piece_count[piece_val] -= 1
-    
-    piece_types_left = {k for k,v in expected_piece_count if v > 0}
-
-    # List containing location of every piece on `team`
+    # List containing location of every piece on `team` at the end of this move
     piece_locs = zip(*np.where(board*team > 0))
 
-    rmoves = []
+    moves = []
+    boards = set()
     for row, col in piece_locs:
-        if on_board(board, row, col) == team*N: 
-            rmoves += knight_move(board, row, col, team)
-        elif on_board(board, row, col) == team*Q:
-            rmoves += normal_move(board, row, col, team, 
+        piece = on_board(board, row, col)
+        
+        # All moves where a Knight takes another piece
+        if piece == team*N: 
+            # All possible moves from ENDSPACE to STARTSPACE
+            # ENDSPACE is the location of an enemy piece at the end of move
+            tmp_moves = knight_move(board, row, col, team)
+            moves = [(move, (N, row, col)) for move in tmp_moves]
+
+        # All moves where a Queen takes another piece
+        elif piece == team*Q:
+            tmp_moves = normal_move(board, row, col, team, 
                     [(-1,-1), (-1,1), (1,-1), (1,1), (1,0), (0,1), (-1,0), 
                         (0,-1)])
-        elif on_board(board, row, col) == team*B:
-            rmoves += normal_move(board, row, col, team, 
+            moves = [(move, (Q, row, col)) for move in tmp_moves]
+
+        # All moves where a Bishop takes another piece
+        elif piece == team*B:
+            tmp_moves = normal_move(board, row, col, team, 
                     [(-1,-1), (-1,1), (1,-1), (1,1)])
-        elif on_board(board, row, col) == team*R:
-            rmoves += normal_move(board, row, col, team, 
+            moves = [(move, (B, row, col)) for move in tmp_moves]
+
+        # All moves where a Rook takes another piece
+        elif piece == team*R:
+            tmp_moves = normal_move(board, row, col, team, 
                     [(0,-1), (0,1), (-1,0), (1,0)])
+            moves = [(move, (R, row, col)) for move in tmp_moves]
+        
+        # All moves where a 
+        if row == backrank[-team]:
+            
+
+    for start, end in moves:
+        start_d, start_r, start_c = start
+        end_d, end_r, end_c = end
+        preboard = board.copy()
+        preboard[start_c-1, start_r-1] = empty
+        preboard[end_c-1, end_r-1] = team*end_d
+        boards.add(preboard)
     
-    possible_moves = []
+    special_moves = {'OO', 'OOO', 'enpassant', 'promotion'}
+    return moves, boards
+
+def backwards_move_to_board(board, start, end):
+
+if __name__ == '__main__':
+    verbosity = int(argv[1]) if len(argv) > 1 else 0
+    games = only_correct_games(fname, start_count = 0, max_count=1, verbose=verbosity)
     
-    return moves
+    correct = 0
+    total = 0
+    
+    for start, end, team, index in gen_pairs(games):
+        start in retrograde(end)
+
+    print("finished: ", correct , " / " , total)
+    
+
+    
